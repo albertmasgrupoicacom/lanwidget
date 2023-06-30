bdb7dfef97e84aac8361fda299c66488fc6f26f6
Sergio Asensio <sergio.asensio@grupoica.com>
2023-06-30 13:07:01 +0200

    REFACTOR: [main] - Main page refactored

--- src/home-chart.js
+++ src/home-chart.js
@@ -13,13 +13,10 @@ export class HomeChart {
     this._helpers = new Helpers();
     // this._exportUtils = new ResultExport();
     this.data;
-    // this.operacionesSelectedTable = 'cruce';
-    // this.cruceSelectedTable = 0;
-    // this.show_legend = true;
+    this.chart;
   }
   
   init(){
-    this.removeAllContainers();
     this.data = avances;
     // this._http.get(`${base_url}/avances`).then(data => {
     //   this.data = data;
@@ -27,138 +24,78 @@ export class HomeChart {
     // }).catch(error => {
     //   console.error('Error', error);
     // });
-    if(this.data && this.data.success) {this.printContainer(this.data)};
+    if(this.data && this.data.success) {this.printTabs(this.data.lista)};
   }
 
-  printContainer(data){
-    const page = document.getElementById('graph_page');
-    let container = document.createElement('div');
-    container.id = `graph_container_0`;
-    page.appendChild(container);
-    // GET FIRST DATA
-    let listaTabs = data.lista;
-    this.printTabs(listaTabs,container);
-  }
-
-  printChart(data, tableIndex,tabContent){
-    let config = {type: data.type, title: data.title};
-    const table = document.getElementById(`canvas_graph`);
-    let canvas = document.createElement("canvas");
-    canvas.id = `graph_chart_${tableIndex}`;
-    canvas.classList.add('graph_chart')
-    tabContent.insertAdjacentElement('beforeend', canvas);
-    new Chart(canvas, {
-      type: config && config.type ? config.type : 'bar',
-      data: data,
-      options: {
-        indexAxis: config && config.axis ? config.axis : 'x',
-        plugins: {
-          title: {
-            display: true,
-            text: data.titulo,
-            position: 'top',
-            color: '#005767',
-            font: {size: 16}
-          },
-          legend: {
-            display: this.show_legend,
-            position: 'bottom',
-          },
-        },
-        responsive: true,
-        scales: {
-          x: {stacked: config && config.stacked != undefined ? config.stacked : false},
-          y: {beginAtZero: true, stacked: config && config.stacked != undefined ? config.stacked : false},
-        },
-      },
-    });
-    const container = document.getElementById(`graph_container_${tableIndex}`);
-    container.setAttribute('config', JSON.stringify(config));
-    this.printChartSelectionButtons(data, tableIndex);
-  }
-
-  printChartSelectionButtons(data, tableIndex){
-    const chart = document.getElementById(`graph_chart_${tableIndex}`);
-    let buttonsContainer = document.createElement('div');
-    buttonsContainer.id = `graph_chart_${tableIndex}_buttons`;
-    resultButtons.forEach(config => {
-      let button = document.createElement('button');
-      button.classList.add('graphic_btn', `graph_chart_${tableIndex}_button`);
-      button.style.background = `url(${config.icon}) no-repeat`;
-      button.onclick = () => {
-        this.removeChart(tableIndex);
-        this.printChart(data, tableIndex, config);
-      }
-      buttonsContainer.appendChild(button);
-    });
-    buttonsContainer.classList.add('my-3', 'd-flex', 'justify-content-end'); 
-    chart.insertAdjacentElement('afterend', buttonsContainer);
-  }
-
-  removeAllContainers() {
-    document.getElementById('graph_page').innerHTML = '';
-  }
-
-  removeChart(tableIndex) {
-    document.getElementById(`graph_chart_${tableIndex}`).remove();
-    document.getElementById(`graph_chart_${tableIndex}_buttons`).remove();
-  }
-
-  printTabs(listaTabs, container) {
+  printTabs(tabList) {
+    const container = document.getElementById('graph_page');
     container.innerHTML = `
-    <div class="tab-container">
-      <ul class="tab-navigation"></ul>
-      <div class="tab-content"></div>
+    <div class="fd-gris-fondo py-5">
+      <div class="container">
+        <div class="tabs-container">
+          <nav class="tabs tab-main">
+            <ul class="nav nav-tabs" id="MainTab" role="tablist"></ul>
+          </nav>
+        </div>
+    
+        <div class="tab-content" id="MainTabContent">
+          <div aria-labelledby="tab-main" class="tab-pane fade show active" id="TabMain" role="tabpanel">
+            <div class="row order-2 order-lg-1 cont-dest-tabs">
+              <div class="col-lg-3 col-xl-3">
+                <div class="tabs-container-sec">
+                  <nav class="tabs tab-sec">
+                    <ul class="nav nav-tabs" id="SecTab" role="tablist"></ul>
+                  </nav>
+                </div>
+              </div>
+    
+              <div class="col-lg-9 col-xl-9">
+                <div class="tab-content" id="SecTabContent">
+                  <canvas id="tab-content-chart"></canvas>
+                </div>
+              </div>
+            </div>
+    
+            <div class="dest-tabs order-1 order-lg-2">
+              <div>
+                <p>"Ya puede acceder al estudio definitivo del Barometro de febrero"</p>
+              </div>
+              <a href="#">Compartir</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>`;
-    const tabNavigation = container.querySelector(".tab-navigation");
-    const tabContent = container.querySelector(".tab-content");
-    listaTabs.forEach( (tab, index) => {
-      const tabNavItem = document.createElement("li");
-      tabNavItem.textContent = tab.titulo;
-  
-      if (index === 0) {
-        tabNavItem.classList.add("active");
-        // TODO: Add tab content ( list prefguntas & chart)
-        this.addTab(tabContent,tab);
-      }
-  
-      tabNavItem.addEventListener("click", () => {
-        const tabNavItems = document.querySelectorAll(".tab-navigation li");
-        tabNavItems.forEach((item) => {
-          item.classList.remove("active");
-        });
-        tabNavItem.classList.add("active");
-        this.addTab(tabContent,tab);
-      });
-      tabNavigation.appendChild(tabNavItem);
-    });
+    this.printMainTab(tabList)
   }
 
-  addTab(tabContent,tab) {
-    const listItems = tab.preguntas.map((item,index) => `<li class="list-item" data-index="${index}">${item.titulo}</li>`).join("");
-    tabContent.innerHTML = `
-                            <ul>${listItems}</ul>
-                            <div class="canvas_graph"></div>`;
-
-    const listElements = document.querySelectorAll(".list-item");
-    listElements.forEach((pregunta,index) => {
-      pregunta.addEventListener("click", (selectedElement,index) => {
-        listElements.forEach((item) => {
-          item.classList.remove("selected");
-        });
-        pregunta.classList.add("selected");
-        const indice = pregunta.dataset.index;
-        const data = tab.preguntas[indice];
-        console.log("Element seleccionat:", pregunta.innerText);
-        console.log("ELemento a Pintar:", data);
-        console.log('Indice:',indice)
-        //TODO: PrintChar
-        this.printChart(this.getParsedData(data),0,tabContent);
-      });
+  printMainTab(tabList){
+    let mainTab = document.getElementById('MainTab');
+    tabList.forEach((tab, tabIndex) => {
+      let tabItem = `<li class="nav-item"><button class="nav-link ${tabIndex == 0 ? 'active' : ''}" aria-controls="MainTab${tabIndex}" aria-selected="${tabIndex == 0 ? 'true' : 'false'}" data-bs-toggle="tab" id="${tabIndex}-tab-main" role="tab" type="button">${tab.titulo}</button></li>`;
+      mainTab.insertAdjacentHTML('beforeend', tabItem);
+      document.getElementById(`${tabIndex}-tab-main`).onclick = () => {
+        this.printSecondTab(tab);
+      }
     });
+    this.printSecondTab(tabList[0]);
+  }
+
+  printSecondTab(tab){
+    let secTab = document.getElementById(`SecTab`);
+    secTab.innerHTML = '';
+    tab.preguntas.forEach((question, questionIndex) => {
+      let questionItem = `<li class="nav-item"><button class="nav-link ${questionIndex == 0 ? 'active' : ''}" aria-controls="TabSec${questionIndex}" aria-selected="${questionIndex == 0 ? 'true' : 'false'}" data-bs-toggle="tab" id="${questionIndex}-tab-sec" role="tab">${question.titulo}</button></li>`;
+      secTab.insertAdjacentHTML('beforeend', questionItem);
+      document.getElementById(`${questionIndex}-tab-sec`).onclick = () => {
+        this.printChart(this.getParsedData(question))
+      }
+    });
+    this.printChart(this.getParsedData(tab.preguntas[0]))
   }
 
   getParsedData(data) {
+    console.log(data);
 
     // datasets: [
     //   {
@@ -203,6 +140,31 @@ export class HomeChart {
       cutout: '50%'
     });
     return result;
+  }
+
+  printChart(data){
+    if(this.chart){this.chart.destroy()}
+    this.chart = new Chart('tab-content-chart', {
+      type: 'bar',
+      data: data,
+      options: {
+        indexAxis: 'x',
+        plugins: {
+          title: {
+            display: true,
+            text: data.titulo,
+            position: 'top',
+            color: '#005767',
+            font: {size: 16}
+          },
+          legend: {
+            display: this.show_legend,
+            position: 'bottom',
+          },
+        },
+        responsive: true
+      },
+    });
   }
 
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import { HomeChart } from './home-chart';
+import { HomeChart } from './home-chart'
+import 'bootstrap';
 // import { DataService } from './services/data.service';
 
 export default function main({portletNamespace, contextPath, portletElementId,configuration}) {
@@ -7,6 +8,7 @@ export default function main({portletNamespace, contextPath, portletElementId,co
 
     const node = document.getElementById(portletElementId);
     node.innerHTML =`
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
         <div id="graph_page" class="cis-caja-tot"></div>
     `;
 
